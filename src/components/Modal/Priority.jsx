0750a3b4b3af7526bbed3ad1112a1bc970ec7e5a
aman vijay <aman.vijay@mountblue.tech>
2020-01-17 20:30:00 +0530

    [WIP] Add changes in Modal architechture for priority

--- src/components/Modal/Priority.jsx
+++ src/components/Modal/Priority.jsx
@@ -1,20 +1,60 @@
 import React from 'react';
-import { Menu } from 'antd';
+import { Menu, Icon } from 'antd';
 import '@fortawesome/fontawesome-free';
 
-export const menu = (
+export const priorityMenu = (
   <Menu>
     <Menu.Item key="0">
-      <i className="fas fa-flag"></i>Priority 1
+      {
+        <div className="priority-flag">
+          <Icon
+            type="flag"
+            theme="filled"
+            style={{ color: 'red', fontWeight: 'bold' }}
+          />
+
+          <span>Priority1</span>
+        </div>
+      }
     </Menu.Item>
     <Menu.Item key="1">
-      <i className="fas fa-flag"></i>Priority 2
+      {
+        <div className="priority-flag">
+          <Icon
+            type="flag"
+            theme="filled"
+            style={{ color: 'yellow', fontWeight: 'bold' }}
+          />
+
+          <span>Priority2</span>
+        </div>
+      }
+    </Menu.Item>
+    <Menu.Item key="2">
+      {
+        <div className="priority-flag">
+          <Icon
+            type="flag"
+            theme="filled"
+            style={{ color: 'blue', fontWeight: 'bold' }}
+          />
+
+          <span>Priority3</span>
+        </div>
+      }
     </Menu.Item>
     <Menu.Item key="3">
-      <i className="fas fa-flag"></i>Priority 3
-    </Menu.Item>
-    <Menu.Item key="4">
-      <i className="fas fa-flag"></i>Priority 4
+      {
+        <div className="priority-flag">
+          <Icon
+            type="flag"
+            theme="filled"
+            style={{ color: 'green', fontWeight: 'bold' }}
+          />
+
+          <span>Priority4</span>
+        </div>
+      }
     </Menu.Item>
   </Menu>
 );
